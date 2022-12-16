52708111e8b92fe5b5d2fe4a2470fdc9b3c295c5
Arsh Bhatia <71868070+arshbhatia8@users.noreply.github.com>
2022-12-16 05:46:18 +0530

    Create 232. Implement Queue using Stacks.cpp

---
+++ 232. Implement Queue using Stacks.cpp
@@ -0,0 +1,38 @@
+class MyQueue {
+public:
+    MyQueue() {
+    }
+
+    void push(int x) {
+        stk1.push(x);
+    }
+
+    int pop() {
+        move();
+        int ans = stk2.top();
+        stk2.pop();
+        return ans;
+    }
+
+    int peek() {
+        move();
+        return stk2.top();
+    }
+
+    bool empty() {
+        return stk1.empty() && stk2.empty();
+    }
+
+private:
+    stack<int> stk1;
+    stack<int> stk2;
+
+    void move() {
+        if (stk2.empty()) {
+            while (!stk1.empty()) {
+                stk2.push(stk1.top());
+                stk1.pop();
+            }
+        }
+    }
+};
