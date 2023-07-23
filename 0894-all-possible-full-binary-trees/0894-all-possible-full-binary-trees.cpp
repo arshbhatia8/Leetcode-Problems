bd8c050d8b30ae5a19a1f9c03aadc2908b64bb9e
Arsh Bhatia <71868070+arshbhatia8@users.noreply.github.com>
2023-07-23 08:08:15 +0530

    Time: 134 ms (17.26%), Space: 33.4 MB (49.02%) - LeetHub

---
+++ 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<TreeNode*> allPossibleFBT(int n) {
+        vector<vector<TreeNode*>> f(n + 1);
+        function<vector<TreeNode*>(int)> dfs = [&](int n) -> vector<TreeNode*> {
+            if (f[n].size()) {
+                return f[n];
+            }
+            if (n == 1) {
+                return vector<TreeNode*>{new TreeNode()};
+            }
+            vector<TreeNode*> ans;
+            for (int i = 0; i < n - 1; ++i) {
+                int j = n - 1 - i;
+                for (auto left : dfs(i)) {
+                    for (auto right : dfs(j)) {
+                        ans.push_back(new TreeNode(0, left, right));
+                    }
+                }
+            }
+            return f[n] = ans;
+        };
+        return dfs(n);
+    }
+};
