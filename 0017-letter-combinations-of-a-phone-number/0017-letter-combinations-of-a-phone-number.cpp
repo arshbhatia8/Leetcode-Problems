fa92f98933608f40cc3f6b191e4169e5ae1fb1b9
Arsh Bhatia <71868070+arshbhatia8@users.noreply.github.com>
2023-08-03 21:09:40 +0530

    Time: 3 ms (37.56%), Space: 6.4 MB (55.27%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
-        if(digits.empty()) return {};
-        vector<string> d ={"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        if (digits.empty()) return {};
+        vector<string> d = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
         vector<string> ans = {""};
-        for (auto i: digits) {
-            string s =d[i-'2'];
+        for (auto i : digits) {
+            string s = d[i - '2'];
             vector<string> t;
-            for (auto& a: ans) {
-                for (auto&b : s){
-                    t.push_back(a+b);
+            for (auto& a : ans) {
+                for (auto& b : s) {
+                    t.push_back(a + b);
                 }
             }
             ans = move(t);
