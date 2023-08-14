9dfdfeeb21f4196d6061a374f1ac00aeba3c8795
Arsh Bhatia <71868070+arshbhatia8@users.noreply.github.com>
2023-08-14 15:40:09 +0530

    Time: 1879 ms (5.01%), Space: 0B (100.00%) - LeetHub

---
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -0,0 +1,6 @@
+SELECT activity_date AS day, count(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE
+    activity_date <= '2019-07-27'
+    AND datediff('2019-07-27', activity_date) < 30
+GROUP BY 1;
